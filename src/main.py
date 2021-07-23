fffda04b0c3c34d06bf2a72727264f04435fbccb
keszinaj <kubajaniszek123@gmail.com>
2021-07-23 18:02:51 +0200

    develop protagonist class and add movement

--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 #main file of my game
 import pygame
-from pygame.constants import WINDOWHITTEST
+import os
+
 
 class global_settings(object):
-    def __init__(self):
-        pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.win = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Space Invasion")
+        #load background
+        self.backg = pygame.image.load("../assets/backg.jpeg")
+        self.win.blit(self.backg, (0,0))
+        self.run = True
+    def draw(self):
+        self.win.blit(self.backg, (0,0))
+        
 
-class player(object):
+
+class protagonist(object):
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.vel = 3
         self.flyLeft = pygame.image.load('../assets/playerLeft.png')
         self.flyRight = pygame.image.load('../assets/playerRight.png')
         self.flyCenter = pygame.image.load('../assets/player.png')
+    def draw(self):
+        game.win.blit(self.flyCenter , (self.x, self.y))
+
 
 class enemy(object):
     def __init__(self):
@@ -22,6 +38,34 @@ class enemy(object):
 
 pygame.init()
 
-win = pygame.display.set_mode((650, 480))
-pygame.display.set_caption("First Game")
-bg = pygame.image.load('../assets/backg.jpeg')
+game = global_settings(600, 600)
+def main():
+    def redraw():
+        game.draw()
+        player.draw()
+
+    player = protagonist(15, 15, 99, 75)
+    player.draw()
+    while game.run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game.run = False
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and player.x>0:
+            player.x -= player.vel
+        elif keys[pygame.K_RIGHT] and game.width > player.x +player.width:
+            player.x += player.vel
+        if keys[pygame.K_UP] and player.y>0:
+            player.y -= player.vel
+        elif keys[pygame.K_DOWN]and game.height > player.y+player.height:
+            player.y += player.vel
+        else:
+            pass
+        redraw()
+        pygame.display.update()
+            
+    
+
+main()
