9fcef4534bd40c54771f8bf146a556737653d8dd
keszinaj <kubajaniszek123@gmail.com>
2021-07-23 18:24:53 +0200

    add FPS, comments and write additional ideas

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@
 import pygame
 import os
 
-
+#here i save all most important varibles
 class global_settings(object):
     def __init__(self, width, height):
+        #size of window
         self.width = width
         self.height = height
+        #window
         self.win = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Space Invasion")
+        self.clock = pygame.time.Clock()
         #load background
         self.backg = pygame.image.load("../assets/backg.jpeg")
-        self.win.blit(self.backg, (0,0))
-        self.run = True
+        self.win.blit(self.backg, (0,0)) #hmm should i delete it?
+        self.run = True #hmmm
+    # to redraw the windows
     def draw(self):
         self.win.blit(self.backg, (0,0))
         
@@ -20,14 +24,19 @@ class global_settings(object):
 
 class protagonist(object):
     def __init__(self, x, y, width, height):
+        # x y to spawn protagonist
         self.x = x
         self.y = y
+        # height and width of protagonist
         self.width = width
         self.height = height
+        #how much on x and y protagonist fly
         self.vel = 3
+        #skins for protagonist
         self.flyLeft = pygame.image.load('../assets/playerLeft.png')
         self.flyRight = pygame.image.load('../assets/playerRight.png')
         self.flyCenter = pygame.image.load('../assets/player.png')
+    #draw protagonist
     def draw(self):
         game.win.blit(self.flyCenter , (self.x, self.y))
 
@@ -37,20 +46,24 @@ class enemy(object):
         pass
 
 pygame.init()
-
 game = global_settings(600, 600)
+
 def main():
+    #redraw window
     def redraw():
         game.draw()
         player.draw()
 
     player = protagonist(15, 15, 99, 75)
     player.draw()
+    #main loop
     while game.run:
+        game.clock.tick(60)#fps
+        #handle quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.run = False
-
+        #handle keys
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] and player.x>0:
@@ -63,6 +76,7 @@ def main():
             player.y += player.vel
         else:
             pass
+        
         redraw()
         pygame.display.update()
             
